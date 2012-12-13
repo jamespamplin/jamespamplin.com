f1762bcbca4d53a1a385d8aeb55df9c2c3016ebc
James Pamplin <james@jamespamplin.com>
2012-12-13 22:50:43 +0000

    Express.js init config setup

--- web.js
+++ web.js
@@ -1,10 +1,40 @@
-var express = require('express');
+var express = require('express'),
+    stylus = require('stylus'),
+    nib = require('nib'),
 
-var app = express.createServer(express.logger());
+    app = express(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+	res.render(
+		'about',
+		{
+			debug: false,
+			meta: require('./content/about.meta.json'),
+			content: '' // TODO: load markdown
+		}
+	);
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
