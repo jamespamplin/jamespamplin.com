ff8bb81ec2d431b8fa063600414f4837f7e6d7ad
James Pamplin <james@eeveebank.com>
2017-11-19 20:14:51 +0000

    Remove poet and blog

--- web.js
+++ web.js
@@ -5,32 +5,9 @@ var express = require('express'),
     jade = require('jade'),
     sass = require('node-sass'),
 
-    Poet = require('poet'),
-
     moment = require('moment');
 
 app.use(morgan('combined'));
-
-var poet = Poet(app, {
-      posts: './posts/',
-      postsPerPage: 5,
-      metaFormat: 'json'
-    });
-
-if (process.env.NODE_ENV !== 'production') {
-  poet = poet.watch();
-}
-
-poet.init().then(function () {
-
-  var post;
-  for (var postId in poet.posts) {
-    post = poet.posts[postId];
-    post.isoDate = moment(post.date).format('YYYY-MM-DD');
-    post.dateFormatted = moment(post.date).format('DD MMMM YYYY');
-  }
-
-});
 
 // node-sass filter hacked into Jade
 // TODO: PR ForbesLindesay/transformers
@@ -57,13 +34,6 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.render('index', {
-    title: 'Blog',
-    contentSchema: 'http://schema.org/Blog'
-  });
-});
-
-app.get('/about', function (req, res) {
     var metadata = require('./content/about.meta.json');
     res.render(
         'about',
@@ -74,8 +44,6 @@ app.get('/about', function (req, res) {
         }
     );
 });
-
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
